9608629a9cc52155277f4282bd567c3ddddaa49f
Mathilde Chollon <mathilde.chollon@gmail.com>
2025-11-26 16:29:11 +0100

    load files in database only if empty

--- app.R
+++ app.R
@@ -15,12 +15,26 @@ tryCatch({
         password = "visualisation"
     )
     message("Connected successfully")
-    message("Reading Parquet file...")
-    df <- read_parquet("data/donnees-2024-reg94.parquet")
-    df_data_frame <- as.data.frame(df)
-    message("Writing to Database...")
-    dbWriteTable(conn, "equipment_access", df_data_frame, overwrite = TRUE, row.names = FALSE)
-    message("Success! Data loaded.")
+    is_empty <- function(con, table) {
+        tryCatch({
+            query <- sprintf("SELECT COUNT(*) AS n FROM %s;", table)
+            n <- dbGetQuery(con, query)$n
+            return(n == 0)}, 
+            error = function(e){return (TRUE)})
+        
+    }
+
+    if(is_empty(conn, "equipment_access")){
+        message("Reading Parquet file...")
+        df <- read_parquet("data/donnees-2024-reg94.parquet")
+        df_data_frame <- as.data.frame(df)
+        message("Writing to Database...")
+        dbWriteTable(conn, "equipment_access", df_data_frame, overwrite = TRUE, row.names = FALSE)
+        message("Success! Data loaded.")
+    }else{
+        message("Database already loaded, skipping data load")
+    }
+    
     dbDisconnect(conn)
 
 }, error = function(e) {
