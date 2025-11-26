30e54488ddaec31bf38175dca8f1eb2557900a8e
vjonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2025-11-26 15:29:53 +0100

    load parquet files + push data in database

--- src/server.R
+++ src/server.R
@@ -1,19 +1,7 @@
 library(shiny)
 
 server <- function(input, output) {
-    tryCatch({
-        conn <- dbConnect(
-            RPostgres::Postgres(),
-            dbname = "shiny_db",
-            host = "127.0.0.1",
-            port = 5432,
-            user = "visualisation",
-            password = "visualisation"
-        )
-        message("Connected successfully")
-    }, error = function(e) {
-        message("DB ERROR: ", e$message)
-    })
+    
     output$map_background <- renderLeaflet({
             leaflet() %>%
                 addTiles()
